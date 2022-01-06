9f12ef3d6ba38c8bf50cf4f5352d0f7988dd0652
3s777 <3s777@rambler.ru>
2022-01-06 21:28:20 +0300

    add task 8

--- task_8.php
+++ task_8.php
@@ -47,50 +47,26 @@
                                         </tr>
                                     </thead>
                                     <tbody>
-                                        <tr>
-                                            <th scope="row">1</th>
-                                            <td>Mark</td>
-                                            <td>Otto</td>
-                                            <td>@mdo</td>
-                                            <td>
-                                                <a href="show.php?id=" class="btn btn-info">Редактировать</a>
-                                                <a href="edit.php?id=" class="btn btn-warning">Изменить</a>
-                                                <a href="delete.php?id=" class="btn btn-danger">Удалить</a>
-                                            </td>
-                                        </tr>
-                                        <tr>
-                                            <th scope="row">2</th>
-                                            <td>Jacob</td>
-                                            <td>Thornton</td>
-                                            <td>@fat</td>
-                                            <td>
-                                                <a href="show.php?id=" class="btn btn-info">Редактировать</a>
-                                                <a href="edit.php?id=" class="btn btn-warning">Изменить</a>
-                                                <a href="delete.php?id=" class="btn btn-danger">Удалить</a>
-                                            </td>
-                                        </tr>
-                                        <tr>
-                                            <th scope="row">3</th>
-                                            <td>Larry</td>
-                                            <td>the Bird</td>
-                                            <td>@twitter</td>
-                                            <td>
-                                                <a href="show.php?id=" class="btn btn-info">Редактировать</a>
-                                                <a href="edit.php?id=" class="btn btn-warning">Изменить</a>
-                                                <a href="delete.php?id=" class="btn btn-danger">Удалить</a>
-                                            </td>
-                                        </tr>
-                                        <tr>
-                                            <th scope="row">4</th>
-                                            <td>Larry the Bird</td>
-                                            <td> Bird</td>
-                                            <td>@twitter</td>
-                                            <td>
-                                                <a href="show.php?id=" class="btn btn-info">Редактировать</a>
-                                                <a href="edit.php?id=" class="btn btn-warning">Изменить</a>
-                                                <a href="delete.php?id=" class="btn btn-danger">Удалить</a>
-                                            </td>
-                                        </tr>
+
+                                        <?php
+                                            $db = new PDO('mysql:host=localhost;dbname=tasks','root','');
+                                            $sql = "SELECT * FROM users";
+                                            $result = $db->query($sql);
+                                            $users = $result->fetchAll(PDO::FETCH_ASSOC);
+
+                                            foreach ($users as $key=>$user) { ?>
+                                                <tr>
+                                                    <th scope="row"><?php echo $key+1; ?></th>
+                                                    <td><?php echo $user['first_name']; ?></td>
+                                                    <td><?php echo $user['last_name']; ?></td>
+                                                    <td><?php echo $user['username']; ?></td>
+                                                    <td>
+                                                        <a href="show.php?id=<?php echo $user['id']?>" class="btn btn-info">Редактировать</a>
+                                                        <a href="edit.php?id=<?php echo $user['id']?>" class="btn btn-warning">Изменить</a>
+                                                        <a href="delete.php?id=<?php echo $user['id']?>" class="btn btn-danger">Удалить</a>
+                                                    </td>
+                                                </tr>
+                                            <?php } ?>
                                     </tbody>
                                 </table>
                             </div>
